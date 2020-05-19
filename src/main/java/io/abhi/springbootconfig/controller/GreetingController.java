a9b947a8c4a994d0a35220e788a3a49535f917a5
abhimana06 <abhishek@Alephs-MacBook-Pro.local>
2020-05-19 12:49:12 +0800

    requestMapping

--- src/main/java/io/abhi/springbootconfig/controller/GreetingController.java
+++ src/main/java/io/abhi/springbootconfig/controller/GreetingController.java
@@ -8,12 +8,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.abhi.springbootconfig.configuration.DBConfig;
 
 @RestController
 @RefreshScope
+@RequestMapping("/configurations")
 public class GreetingController {
 	
 	@Value("${my.greetings}")
